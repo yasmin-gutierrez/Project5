7bc5046d060da607cef897e6ea8d00e9a32ac8a1
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-28 15:48:18 -0600

    updated nav bar

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,13 +1,27 @@
+<script setup>
+import { useAuth } from '../composables/useAuth'
+const { isAuthenticated, logout, user } = useAuth()
+</script>
+
 <template>
-  <nav  class="bg-slate-900 h-20 items-center text-white">
+  <nav class="bg-slate-900 h-20 items-center text-white flex justify-center space-x-4">
     <RouterLink :to="{name: 'Home'}">Home</RouterLink>
     <RouterLink :to="{name: 'Other'}">Other</RouterLink>
-    <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+    
     <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
+
+    <div v-if="isAuthenticated">
+      Welcome {{ user.email }}
+      <button @click="logout" class="rounded-md bg-red-500 px-4 py-2 mx-2 text-red-100 hover:bg-red-700">LOGOUT</button>
+    </div>
+    <div v-else>
+      <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+    </div>
   </nav>
 </template>
 
-<style lang="postcss" scoped>
+
+<!--<style lang="postcss" scoped>
 nav {
   @apply flex justify-center space-x-4;
 
@@ -15,4 +29,4 @@ nav {
     @apply underline underline-offset-4;
   }
 }
-</style>
+</style>-->
