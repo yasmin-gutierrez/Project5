b2c80c539725e6ebbeffdb8830e38e6c030c0d51
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-28 15:59:46 -0600

    pagination working

---
+++ src/components/MainPagination.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+const pages = ref(10)
+const activePage = ref(2)
+
+const prevPage = () => {
+    if (activePage.value > 1) {
+        activePage.value--
+    }
+    console.log(activePage.value)
+}
+
+const nextPage = () => {
+    if (activePage.value < pages.value) {
+        activePage.value++
+    }
+    console.log(activePage.value)
+
+}
+
+
+</script>
+
+<template>
+    <div class="flex justify-center gap-4 p-8">
+
+        <button @click="prevPage" :disabled="activePage === 1" class="">PREV</button>
+
+        <button v-for="page in pages" :key="page" > {{ page }}</button>
+
+        <button @click="nextPage" :disabled="activePage === pages" class="">NEXT</button> 
+
+    </div>
+
+</template>
