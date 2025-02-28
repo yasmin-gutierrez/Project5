f0298f88eb01cbf901f8f2dacc424ff3fc3a4430
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-28 13:07:45 -0600

    Card Componets Working

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import { faker } from '@faker-js/faker'
+    const firstname = faker.person.firstName()
+    const lastname = faker.person.lastName()
+    
+</script>
+
+<template>
+    {{ fistname }} {{ lastname }} <br/>
+</template>
