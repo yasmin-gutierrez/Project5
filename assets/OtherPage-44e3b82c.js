f44b19910b840d5e36021081be9ff989621d08ed
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-03-01 21:55:41 -0600

    adding dist

---
+++ assets/OtherPage-44e3b82c.js
@@ -0,0 +1,1 @@
+import{_ as t,o as s,c as n,a as r}from"./index-58e3604d.js";const a={},c={class:"flex min-h-screen items-center justify-center"};function o(f,e){return s(),n("main",c,e[0]||(e[0]=[r("h1",{class:"text-6xl font-thin text-slate-800"},"I'm Other Page",-1)]))}const l=t(a,[["render",o]]);export{l as default};
